dff2ed13daa1560af948b4976b6e86b04ab2cf17
rosmildoLopes <[200~123316625+rosmildoLopes@users.noreply.github.com~>
2024-08-08 23:09:51 -0300

    new projects

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -5,13 +5,16 @@ import Head from "next/head";
 import React from "react";
 import Image from "next/image";
 import { GithubIcon } from "../components/Icons";
-import project1 from "../../public/images/projects/project1.png";
-import project2 from "../../public/images/projects/project2.png";
-import project4 from "../../public/images/projects/project4.png";
-import project8 from "../../public/images/projects/project8.png";
-import project3 from "../../public/images/projects/project3.png";
-import project5 from "../../public/images/projects/project5.png";
-import project6 from "../../public/images/projects/project6.png";
+import gpt3Blog from "../../public/images/projects/gpt3Blog.svg";
+import bankApp from "../../public/images/projects/bankApp.svg";
+import academiaNumen from "../../public/images/projects/academiaNumen.svg";
+import todoApp from "../../public/images/projects/todoApp.svg";
+import turnero from "../../public/images/projects/turnero.svg";
+import cuyanasForm from "../../public/images/projects/cuyanasForm.svg";
+import calendarioAcademia from "../../public/images/projects/calendarioAcademia.svg";
+import sofiLanding from "../../public/images/projects/sofiLanding.svg";
+import tutorias from "../../public/images/projects/tutorias.svg";
+import mutsepu from "../../public/images/projects/mutsepu.svg";
 
 import TransitionEffect from "@/components/TransitionEffect";
 
@@ -139,79 +142,118 @@ const projects = () => {
         <Layout className="pt-16">
           <AnimatedText
             className="mb-16 lg:!text-7xl sm:mb-8 sm:!text-6xl xs:!text-4xl"
-            text="¡Conoce algunos de mis proyectos!"
+            text="¡Take a look on some of my projects!"
           />
 
-          <div className="grid grid-cols-12 gap-24 gap-y-32 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm:gap-x-0">
+          <div className="grid grid-cols-12 gap-24 gap-y-24 xl:gap-x-12 lg:gap-x-8 md:gap-y-12 sm:gap-x-0">
             <div className="col-span-12">
               <FeaturedProject
-                type="Featured Project"
-                title="Billing App"
-                img={project1}
-                summary="Built with React and Tailwind CSS, utilizing the latest features and following 
-                best practices for clean code."
-                link="https://modern-website-tau.vercel.app/"
-                github="https://github.com/rosmildoLopes/modern-website"
+                type="Academia Numen"
+                title="Landing Page"
+                img={academiaNumen}
+                summary="I got the chance to build their page with a nice and clean design using the latest technologies"
+                link="https://www.academianumen.com.ar/"
+                github="https://github.com/NumenPublicidad/landingAcademia"
               />
             </div>
-            <div className="col-span-6 sm:col-span-12">
-              <Projects
-                type="Project"
-                title="HTML || CSS Calculator"
-                img={project5}
-                summary="Calculator"
+            <div className="col-span-12">
+              <FeaturedProject
+                type="Inversiones Cuyanas"
+                title="Multistep Form"
+                img={cuyanasForm}
+                summary="A complete multistep form with zod validation in each step, react-hook-form, images uploading,email sending using resend and react email and integration with appwrite database and storage"
+                link="https://landing-academia.vercel.app/"
+                github="#"
+              />
+            </div>
+            <div className="col-span-12">
+              <FeaturedProject
+                type="Municipio Malvinas"
+                title="CRUD Application"
+                img={turnero}
+                summary="Online processing of administrative tasks, streamlining procedures such as licenses, permits, and municipal registrations.A page to create appointment to driver's license integrate with backend. You can create an appointment, remind appointment,etc. Admin panel to advanced settings"
                 link="https://rosmildolopes.github.io/Calculator/"
-                github="https://github.com/rosmildoLopes/Calculator"
+                github="https://github.com/NumenPublicidad/turnero"
               />
             </div>
-            <div className="col-span-6 sm:col-span-12">
-              <Projects
-                type="Project"
-                title="To do List"
-                img={project6}
-                summary=""
+            <div className="col-span-12">
+              <FeaturedProject
+                type="Calandar Project"
+                title="Fullstack Application"
+                img={calendarioAcademia}
+                summary="A complete CRUD application to manage a full team of work. Created to a company that sells course of technologies. The app creates new courses, new users, registers new teachers,  manages the whole organization to a better project management"
+                link="https://numencalendar.vercel.app/login"
+                github="https://github.com/NumenPublicidad/FrontCalendar"
+              />
+            </div>
+            <div className="col-span-12">
+              <FeaturedProject
+                type="SoFi Industries"
+                title="Landing Page"
+                img={sofiLanding}
+                summary="Built with Next and Tailwind CSS, a simply landing page to a tech Company"
+                link="https://sofi-landing-bice.vercel.app/"
+                github="https://github.com/NumenPublicidad/SOFI-landing"
+              />
+            </div>
+            <div className="col-span-12">
+              <FeaturedProject
+                type="Health Care"
+                title="CRUD Application"
+                img={cuyanasForm}
+                summary="Auth authentication, appwrite integration, create appointment, this project is a personal project that is process yet"
                 link="https://todo-list-rho-flax.vercel.app/"
                 github="https://github.com/rosmildoLopes/todo-list"
               />
             </div>
             <div className="col-span-12">
               <FeaturedProject
-                type="Featured Project"
-                title="GPT-3 BLOG"
-                img={project4}
-                summary="A React application that implements various components and features using React and Vanilla CSS."
+                type="GPT-3 BLOG"
+                title="Landing Page"
+                img={mutsepu}
+                summary="A landing page to a friends coorporation."
+                link="https://mutsepu.vercel.app/"
+                github="https://github.com/NumenPublicidad/mutsepu-"
+              />
+            </div>
+            <div className="col-span-12">
+              <FeaturedProject
+                type="Billing App"
+                title="Landing Page"
+                img={bankApp}
+                summary="Made with vite and tailwing used to improve my skills and learnings"
+                link="https://modern-website-tau.vercel.app/"
+                github="https://github.com/rosmildoLopes/modern-website"
+              />
+            </div>
+            <div className="col-span-12">
+              <FeaturedProject
+                type="Project"
+                title="Password Generator"
+                img={gpt3Blog}
+                summary="Simply description about the project 0that I made using aan API with ReactJS"
                 link="https://gpt3-blog.vercel.app/"
                 github="https://github.com/rosmildoLopes/gpt3-blog"
               />
             </div>
-            <div className="col-span-6 sm:col-span-12">
-              <Projects
-                type="Project"
-                title="Weather App"
-                img={project2}
-                summary="Made with Next"
-                link="https://next-weather-app-bice.vercel.app/"
-                github="https://github.com/rosmildoLopes/next-weather-app"
-              />
-            </div>
-            <div className="col-span-6 sm:col-span-12">
-              <Projects
+            <div className="col-span-12">
+              <FeaturedProject
                 type="Project"
                 title="Password Generator"
-                img={project3}
-                summary="Simply description about the project 0that I made using aan API with ReactJS"
-                link="https://password-generator-teal-eight.vercel.app/"
-                github="https://github.com/rosmildoLopes/generate-password"
+                img={todoApp}
+                summary="Vainilla Javascript application that create and delete tasks"
+                link="https://todo-list-rho-flax.vercel.app/"
+                github="https://github.com/rosmildoLopes/todo-list"
               />
             </div>
-            <div className="col-span-6 sm:col-span-12">
-              <Projects
-                type="Project"
-                title="React - Shopping Cart"
-                img={project8}
-                summary="Shopping Cart"
-                link="https://rosmildolopes.github.io/react-shoppingcart/"
-                github="https://github.com/rosmildoLopes/react-shoppingcart"
+            <div className="col-span-12">
+              <FeaturedProject
+                type="Worrkshops"
+                title="Landing Page"
+                img={tutorias}
+                summary="Vainilla Javascript application that create and delete tasks"
+                link="https://tutorias-programacion.vercel.app/"
+                github="https://github.com/NumenPublicidad/tutorias-programacion"
               />
             </div>
           </div>
